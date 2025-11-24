9d07771391b75a1a19893333ab47c518ead20ff9
kjes4pres <stangelandkjersti@gmail.com>
2025-11-24 14:58:38 +0100

    more activations

--- Code/functions/nn_pytorch.py
+++ Code/functions/nn_pytorch.py
@@ -24,6 +24,8 @@ class WeatherNN(nn.Module):
             "relu": nn.ReLU(),
             "lrelu": nn.LeakyReLU(),
             "sigmoid": nn.Sigmoid(),
+            "tanh": nn.Tanh(),
+            "elu": nn.ELU()
         }
         act = activations[activation.lower()]
 
