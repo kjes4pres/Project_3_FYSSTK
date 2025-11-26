fixed possible bug in derivative?

Code/functions/cost_functions.py
@@ -4,4 +4,4 @@ def cross_entropy(predict, target):
     return -np.sum(target*np.log(predict))
 
 def cross_entropy_der(predict, target):
-    return target/predict*(-1)
+    return (predict - target)
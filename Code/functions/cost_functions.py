9ac8c29555d327560f34223b8d0c3aeebace693a
kjes4pres <stangelandkjersti@gmail.com>
2025-11-26 13:49:45 +0100

    fixed possible bug in derivative?

--- Code/functions/cost_functions.py
+++ Code/functions/cost_functions.py
@@ -4,4 +4,4 @@ def cross_entropy(predict, target):
     return -np.sum(target*np.log(predict))
 
 def cross_entropy_der(predict, target):
-    return target/predict*(-1)
+    return (predict - target)
